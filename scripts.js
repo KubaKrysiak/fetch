84f943548250ec82ca4d2c83d89c624dafe6d458
KubaKrysiak <43858173+KubaKrysiak@users.noreply.github.com>
2020-01-02 19:46:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
-(async function(){
-	const request = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
-	const response = await req.json();
-	console.log(res);
-  })()
+const pokemonImg = document.querySelector('#pokemonImg')
+const pokemonName = document.querySelector('#pokemonName')
+const pokemonStats = document.querySelector('#pokemonStats')
+
+async function getPokemon(name){
+	const request = await fetch('https://pokeapi.co/api/v2/pokemon/'+name+'/')
+	const response = await request.json();
+	return response;
+  }
+
+  function drawPokemon(pokemon) {
+	pokemonImg.srcset = pokemon.spirites.front_default;
+	pokemonName.innerText = pokemon.name;
+	pokemonStats.innerHTML = pokemon.stats.map(el => {
+		return `<li>${el.stat.name}: ${el.base_stat}</li>`;
+
+	}).join('');
+  }
+
+  async function load (name){
+	const pokemon = await getPokemon (name);
+	drawPokemon(pokemon)
+
+  }
+  async function start(){
+	load('charizard')
+}
+start();
